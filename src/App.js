6a431a6b3f7cea97a21c9d693ae29e9531a3839f
SamaneM <samanemalmir73@gmail.com>
2024-02-17 22:36:51 -0500

    add basic elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionsList from './components/TransactionsList';
+import AddTransaction from './components/AddTransaction';
 
 function App() {
   return (
     <div>
-
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionsList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
