d46ff697a701d0d19acc8ff1afda81d969bd172d
SamaneM <samanemalmir73@gmail.com>
2024-02-17 23:15:45 -0500

    create transaction component

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,5 +1,5 @@
 import React, {createContext, useReducer} from "react";
-
+import AppReducer from './AppReducer';
 
 
 const initialState = {
@@ -16,7 +16,7 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    return(<GlobalContext.Provider>
+    return(<GlobalContext.Provider value={{ transactions: state.transactions}}>
         {children}
     </GlobalContext.Provider>);
   
