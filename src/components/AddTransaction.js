d4d18197d9082a53f1a217452f761192978be674
SamaneM <samanemalmir73@gmail.com>
2024-02-20 22:59:23 -0500

    add transaction type

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -6,6 +6,7 @@ import { collection, addDoc } from 'firebase/firestore';
 const AddTransaction = () => {
     const [text, setText] = useState(''); // text: current state value. setText: function that allows you to update the state value
     const [amount, setAmount] = useState('');
+    const [type, setType] = useState('expense');
 
     const createTransaction = async (e) => {
         e.preventDefault();
@@ -15,7 +16,8 @@ const AddTransaction = () => {
         }
         await addDoc(collection(db, 'expenses'), {
             text,
-            amount: parseFloat(amount),
+            amount: type == "expense" ? -parseFloat(amount) : parseFloat(amount),
+            type: type
         });
         setText('');
         setAmount('');
@@ -30,8 +32,19 @@ const AddTransaction = () => {
                     <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text..." />
                 </div>
                 <div className="form-control">
-                    <label htmlFor="amount">Amount <br />(negative - expense, positive - income)</label>
+                    <label htmlFor="amount">Amount</label>
                     <input type="number" value={amount} onChange={(e) => setAmount(e.target.value)} placeholder="Enter amount..." />
+                </div>
+                <div className="form-control">
+                    <label>Type:</label>
+                    <div>
+                        <input type="radio" id="expense" name="type" value="expense" checked={type === 'expense'} onChange={() => setType('expense')} />
+                        <label htmlFor="expense">Expense</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="income" name="type" value="income" checked={type === 'income'} onChange={() => setType('income')} />
+                        <label htmlFor="income">Income</label>
+                    </div>
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
