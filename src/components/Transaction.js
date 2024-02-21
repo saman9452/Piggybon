4358baf517ddad6d3ef8a474efa3150d1faa373c
SamaneM <samanemalmir73@gmail.com>
2024-02-20 23:38:38 -0500

    add transaction category and date UI

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -8,6 +8,7 @@ import { db } from '../firebase';
 
 
 const Transaction = ({transaction}) => {
+  console.log(transaction);
 
   const sign = transaction.amount < 0 ? '-' : '+';
 
@@ -16,9 +17,25 @@ const Transaction = ({transaction}) => {
       await deleteDoc(doc(db, 'expenses', id));
     };
 
+    // Convert Firestore timestamp object to JavaScript Date object
+    const timestamp = new Date(transaction.date.seconds * 1000);
+
+    // Format the date
+    const formattedDate = timestamp.toLocaleDateString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+    });
+
   return (
     <li className={transaction.amount<0 ? "minus": "plus"}>
-        {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button onClick={ () => deleteTransaction(transaction.id)} className="delete-btn">x</button>
+      <div>
+        {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
+      </div>
+      <div className='subtext'>
+        <span>{transaction.category}</span> <span>{formattedDate}</span>
+      </div>
+      <button onClick={ () => deleteTransaction(transaction.id)} className="delete-btn">x</button>
     </li>
   )
 }
