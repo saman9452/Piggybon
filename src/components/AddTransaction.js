ad25fbf3e699452e5a9272c60c9a8d26202cc44e
SamaneM <samanemalmir73@gmail.com>
2024-02-21 22:07:01 -0500

    add category

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,12 +1,26 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 import { db } from '../firebase';
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
 
 const AddTransaction = () => {
     const [text, setText] = useState(''); // text: current state value. setText: function that allows you to update the state value
     const [amount, setAmount] = useState('');
     const [type, setType] = useState('expense');
+    const [categories, setCategories] = useState([]);
+    const [selectedCategory, setSelectedCategory] = useState('');
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            const categoriesCollection = collection(db, 'categories');
+            const categoriesSnapshot = await getDocs(categoriesCollection);
+            const categoriesData = categoriesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setCategories(categoriesData);
+            setSelectedCategory(categoriesData.length > 0 ? categoriesData[0].id : ''); // Set the default selected category
+        };
+        fetchCategories();
+    }, []);
+
 
     const createTransaction = async (e) => {
         e.preventDefault();
@@ -17,7 +31,8 @@ const AddTransaction = () => {
         await addDoc(collection(db, 'expenses'), {
             text,
             amount: type == "expense" ? -parseFloat(amount) : parseFloat(amount),
-            type: type
+            type: type,
+            category: selectedCategory
         });
         setText('');
         setAmount('');
@@ -45,6 +60,14 @@ const AddTransaction = () => {
                         <input type="radio" id="income" name="type" value="income" checked={type === 'income'} onChange={() => setType('income')} />
                         <label htmlFor="income">Income</label>
                     </div>
+                </div>
+                <div className="form-control">
+                    <label htmlFor="category">Category</label>
+                    <select className="category-dropdown" value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+                        {categories.map(category => (
+                            <option key={category.id} value={category.id}>{category.name}</option>
+                        ))}
+                    </select>
                 </div>
                 <button className="btn">Add transaction</button>
             </form>
