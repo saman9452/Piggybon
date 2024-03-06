9cb2f469cb15cd42b1d444a408a92f9b2c1ca3c9
SamaneM <samanemalmir73@gmail.com>
2024-03-05 23:07:03 -0500

    chnage react version and install firebase packages

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -7,12 +7,6 @@ import rooteducer from './store/reducers/rootReducer';
 
 const store = createStore(rooteducer);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
