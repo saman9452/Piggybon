abbf05821026a55d33147a3fbfdd2ff9eee3180b
SamaneM <samanemalmir73@gmail.com>
2024-03-07 23:23:05 -0500

    update trabsaction reducer

--- src/store/reducers/transactionReducer.js
+++ src/store/reducers/transactionReducer.js
@@ -1,7 +1,16 @@
 const initialState = {};
 
 const transactionReducer = (state = initialState, action) => {
-    return state
-}
+    switch (action.type) {
+        case 'CREATE_PROJECT_SUCCESS':
+            console.log('create project success', action.transaction);
+            return state;
+        case 'CREATE_PROJECT_ERROR':
+            console.log('create project error', action.err);
+            return state;
+        default:
+            return state;
+    }
+};
 
-export default transactionReducer
+export default transactionReducer;
