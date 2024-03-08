fefab47b2ee0d822bbb227c053e58f7b9df65fd6
SamaneM <samanemalmir73@gmail.com>
2024-03-07 22:17:11 -0500

    addthunk middleware and store enhancers

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import rooteducer from './store/reducers/rootReducer';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { reactReduxFirebase, getFirebase } from 'react-redux-firebase';
+import fbConfig from './config/firebase'
 
-const store = createStore(rooteducer);
+const store = createStore(rooteducer, compose(
+    applyMiddleware(thunk.withExtraArgument({getFirebase, getFirestore})), // this is a store enhancer
+    reduxFirestore(fbConfig), 
+    reactReduxFirebase(fbConfig)
+));
 
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
