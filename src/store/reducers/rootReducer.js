cddea196f5414954a72b2370091e7582444f8559
SamaneM <samanemalmir73@gmail.com>
2024-03-08 23:03:55 -0500

    Change Root Reducer react-redux-firebase version 3

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,11 +1,16 @@
+import { combineReducers } from "redux";
+import { firebaseReducer } from "react-redux-firebase";
+import { firestoreReducer } from "redux-firestore";
 import authReducer from "./authReducer";
 import transactionReducer from "./transactionReducer";
 
-import { combineReducers } from "redux";
 
-const rooteducer = combineReducers({
+const rootReducer = combineReducers({
+    firebase: firebaseReducer,
+    firestore: firestoreReducer,
     auth: authReducer,
     transaction: transactionReducer
 });
 
-export default rooteducer;
+export default rootReducer;
+
