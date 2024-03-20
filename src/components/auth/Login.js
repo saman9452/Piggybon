5e54ffbd82b6fec3fc49f92bbc7c790cb96725d5
SamaneM <samanemalmir73@gmail.com>
2024-03-19 23:00:17 -0400

    Add Signed out

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { signIn } from "../../store/actions/authActions";
+import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
-export class login extends Component {
+export class Login extends Component {
   
     state = {
         email: '',
@@ -12,32 +15,52 @@ export class login extends Component {
           [e.target.id]: e.target.value
         })
       }
-      
+
       handleSubmit = (e) => {
         e.preventDefault();
         console.log(this.state);
+        this.props.signIn(this.state);
       }
 
     render() {
-        return (
-        <div className='container'>
-            <form className='white' onSubmit={this.handleSubmit}>
-            <h5 className="grey-text text-darken-3">Sign In</h5>
-            <div className="input-field">
-                <label htmlFor="email">Email</label>
-                <input type="email" id='email' onChange={this.handleChange} />
-            </div>
-            <div className="input-field">
-                <label htmlFor="password">Password</label>
-                <input type="password" id='password' onChange={this.handleChange} />
-            </div>
-            <div className="input-field">
-                <button className="btn pink lighten-1 z-depth-0">Login</button>
-            </div>
-            </form>
-        </div>
-        )
+      const { uid } = this.props;
+      if(uid) return <Redirect to="/"/>
+      return (
+      <div className='container'>
+          <form className='white' onSubmit={this.handleSubmit}>
+          <h5 className="grey-text text-darken-3">Sign In</h5>
+          <div className="input-field">
+              <label htmlFor="email">Email</label>
+              <input type="email" id='email' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+              <label htmlFor="password">Password</label>
+              <input type="password" id='password' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+              <button className="btn primary-btn lighten-1 z-depth-0">Login</button>
+          </div>
+          </form>
+      </div>
+      )
     }
 }
 
-export default login
+const mapStateToProps = (state) => {
+  console.log(state);
+  const uid = state.firebase.auth.uid;
+  return {
+    uid: uid,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signIn: (creds) => dispatch(signIn(creds)),
+  };
+};
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
+
+
--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,16 +1,32 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory   } from 'react-router-dom';
+import firebase from 'firebase/app';
 
 const SignedInLinks = () => {
+  const history = useHistory();
+
+  const handleSignOut = (event) => {
+    event.preventDefault(); // Prevent the default behavior of NavLink
+
+    firebase.auth().signOut()
+      .then(() => {
+        console.log('Signed Out');
+        history.push("/login");
+      })
+      .catch((error) => {
+        console.error('Sign Out Error', error);
+      });
+  };
+
   return (
     <div>
       <ul className="right">
         <li><NavLink to='/'>New Transaction</NavLink></li>
-        <li><NavLink to='/'>Log Out</NavLink></li>
+        <li><NavLink to='/login' onClick={handleSignOut}>Log Out</NavLink></li>
         {/* <li><NavLink to='/' className="btn btn-floating pink lighten-1">Smn</NavLink></li> */}
       </ul>
     </div>
   )
 }
 
-export default SignedInLinks
+export default SignedInLinks;
