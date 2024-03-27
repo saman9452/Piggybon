ac1d874f39ec57c6dcef83ef658b6fd6533dbfbf
SamaneM <samanemalmir73@gmail.com>
2024-03-27 00:26:11 -0400

    Inome Expense Calculation

--- src/components/dashboard/IncomeExpenses.js
+++ src/components/dashboard/IncomeExpenses.js
@@ -1,34 +1,39 @@
-import React, { useContext } from 'react';
-import { GlobalContext } from '../../context/GlobalState';
+import React from 'react';
 
-const IncomeExpenses = () => {
-  const {transactions} = useContext(GlobalContext);
+const IncomeExpenses = ({ transactions }) => {
+  let income = 0;
+  let expense = 0;
   
-  const amounts = transactions.map(transaction => transaction.amount);
+  if (transactions) {
+    
+    const amounts = Object.entries(transactions).map(([id, transaction]) => parseFloat(transaction.amount));
 
-  const income = amounts
-    .filter(item => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
+    console.log(amounts);
+    income = amounts
+      .filter(item => item > 0)
+      .reduce((acc, item) => acc + item, 0)
+      .toFixed(2);
 
-  const expense = (
-    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
-    -1
-  ).toFixed(2);
-
+    expense = (
+      amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => acc + item, 0) *
+      -1
+    ).toFixed(2);
+  }
 
   return (
     <div className="inc-exp-container">
-        <div>
-          <h5>Income</h5>
-          <p className="money plus">+${income}</p>
-        </div>
-        <div>
-          <h5>Expense</h5>
-          <p className="money minus">-${expense}</p>
-        </div>
+      <div>
+        <h5>Income</h5>
+        <p className="money plus">+${income}</p>
       </div>
-  )
-}
+      <div>
+        <h5>Expense</h5>
+        <p className="money minus">-${expense}</p>
+      </div>
+    </div>
+  );
+};
 
-export default IncomeExpenses
+export default IncomeExpenses;
